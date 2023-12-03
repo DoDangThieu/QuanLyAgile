1a7f48e64c1cbc0a9cc75ed13a39e70c60de6597
MeowChill <thehaidam12@gmail.com>
2023-12-04 02:58:32 +0700

    + Đăng nhập form , program = DangNhap , MODEL SQL

---
+++ Models/Monhoc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLyAgile.Models
+{
+    public partial class Monhoc
+    {
+        public string Mamon { get; set; } = null!;
+        public string Tenmon { get; set; } = null!;
+    }
+}
